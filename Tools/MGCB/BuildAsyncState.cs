9e906699a8cf06945921589dfe331841d53abfc1
Nikos Kastellanos <nkastellanos@gmail.com>
2016-09-18 15:12:51 +0300

    Add BuildAsyncState

---
+++ Tools/MGCB/BuildAsyncState.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace MGCB
+{
+    internal class BuildAsyncState
+    {
+        public string SourceFile { get; internal set; }
+        public string OutputFile { get; internal set; }
+        public string Importer { get; internal set; }
+        public string Processor { get; internal set; }
+        public OpaqueDataDictionary ProcessorParams { get; internal set; }
+        public ConsoleAsyncLogger Logger { get; internal set; }
+    }
+}
