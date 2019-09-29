71bac120861c10699964c2b840a5043a3e9b5480
Nikos Kastellanos <nkastellanos@gmail.com>
2019-09-29 17:06:39 +0300

    add Cardboard nuget

--- BuildNuget.bat
+++ BuildNuget.bat
@@ -8,4 +8,6 @@
 "C:\Program Files (x86)\NuGet3\nuget.exe" pack NuGetPackages/MonoGame.Framework.Portable.nuspec			-OutputDirectory NuGetPackages\Output\  -BasePath .  -Version 3.8.9009.0  -Properties Configuration=Release
 "C:\Program Files (x86)\NuGet3\nuget.exe" pack NuGetPackages/MonoGame.Framework.Content.Pipeline.Portable.nuspec -OutputDirectory NuGetPackages\Output\  -BasePath .  -Version 3.8.9009.0  -Properties Configuration=Release
 
+"C:\Program Files (x86)\NuGet3\nuget.exe" pack NuGetPackages/MonoGame.Framework.Cardboard.nuspec        -OutputDirectory NuGetPackages\Output\  -BasePath .  -Version 3.8.9009.0  -Properties Configuration=Release
+
 @pause
