c7c1dce6cae319e9cf776cd72f0b59a6ad3bcb04
Nikos Kastellanos <nkastellanos@gmail.com>
2019-10-06 09:08:00 +0300

    Revert PR-6700
    Forcing Inlilining is not recomented

--- MonoGame.Framework/Point.cs
+++ MonoGame.Framework/Point.cs
@@ -212,7 +212,6 @@ namespace Microsoft.Xna.Framework
         /// Gets a <see cref="Vector2"/> representation for this object.
         /// </summary>
         /// <returns>A <see cref="Vector2"/> representation for this object.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Vector2 ToVector2()
         {
             return new Vector2(X, Y);
