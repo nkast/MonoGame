fde949e6b34833fe88282896b50314236fe02b0c
Nick Kastellanos <nkastellanos@gmail.com>
2020-07-22 23:38:07 +0300

    Add VS templates
    ----
    [ANDROID] fix vs template.
    -Remove OpenTK reference.
    -Add all required ABIs.
    -Update target framwork to v9.0 (Pie)
    -add manifest
    -add ConfigChanges UiMode and SmallestScreenSize
    ----
    
    Add VS2017 Multiplatform template
    Create projects Shared, Android, WindowsDX, desktopGL,WUP.Xaml, WUP.Core
    
    Add VS2019 templates

---
+++ Templates/VisualStudio2019/Multiplatform/DesktopGL/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace $ext_safeprojectname$
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new $ext_safeprojectname$Game())
+                game.Run();
+        }
+    }
+}
