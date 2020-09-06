907a808279c22253afcd726ef5c0f292fc9fb809
Nick Kastellanos <nkastellanos@gmail.com>
2020-09-06 13:14:26 +0300

    [Content.Pipeline] Fix FontImporter kerning

--- MonoGame.Framework.Content.Pipeline/Graphics/Font/SharpFontImporter.cs
+++ MonoGame.Framework.Content.Pipeline/Graphics/Font/SharpFontImporter.cs
@@ -145,6 +145,9 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Graphics
 			abc.A = face.Glyph.Metrics.HorizontalBearingX >> 6;
 			abc.B = face.Glyph.Metrics.Width >> 6;
 			abc.C = (face.Glyph.Metrics.HorizontalAdvance >> 6) - (abc.A + abc.B);
+			var bitmapLeft = face.Glyph.BitmapLeft;
+			abc.A -= bitmapLeft;
+			abc.B += bitmapLeft;
 
 			// Construct the output Glyph object.
 			return new Glyph(character, glyphBitmap)
